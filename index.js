fcdaec4f82b1a62b078dc09cdac7f6934bc17b81
emaphp <emmanuel.antico@gmail.com>
2014-10-17 12:26:30 -0300

    package.json + index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+var _ = require('underscore');
+
+module.exports = function(content) {
+    this.cacheable && this.cacheable();
+    var callback = this.async();
+    var fn = _.template(content);
+    callback(null, "module.exports = " + fn.source + ";");
+}
