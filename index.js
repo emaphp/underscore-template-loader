95e0b5008ae353aeb02cded989c98429cf868ede
emaphp <emmanuel.antico@gmail.com>
2014-10-18 19:49:55 -0300

    wrapping underscore instance in module.exports

--- index.js
+++ index.js
@@ -4,5 +4,7 @@ module.exports = function(content) {
     this.cacheable && this.cacheable();
     var callback = this.async();
     var fn = _.template(content);
-    callback(null, "module.exports = " + fn.source + ";");
-}
+    callback(null, "module.exports = " + fn+ ";");
+};
+
+module.exports._ = _;
