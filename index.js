d3b4aa3d5457f5eb6feb76ec3a311061102c2b53
emaphp <emmanuel.antico@gmail.com>
2015-01-21 11:54:38 -0300

    underscore as peer dependency

--- index.js
+++ index.js
@@ -1,46 +1,44 @@
 var _ = require('underscore');
-var loaderUtils = require('loader-utils');
-var path = require('path');
-var fs = require('fs');
 
-function resolveContent(content, root) {
-    var includeRegex = /<!--include\s+([\/\w\.]*?[\w]+\.[\w]+)-->/g;
-    var matches = includeRegex.exec(content);
+module.exports = function() {
+	var loaderUtils = require('loader-utils');
+	var path = require('path');
+	var fs = require('fs');
+	var includeRegex = /<!--include\s+([\/\w\.]*?[\w]+\.[\w]+)-->/g;
+	
+	var readFile = (filepath, root) {
+		var self = readFile;
+		self.buffer = {} || self.buffer;
+		
+		if (filepath in self.buffer) {
+			return self.buffer[filepath];
+		}
+		
+		var content = resolveContent(fs.readFileSync(path.join(root, filepath), 'utf8'), root);    
+		self.buffer[filepath] = content;
+		return self.buffer[filepath];
+	};
+	
+	var readContent = function(content, root) {    
+		var matches = includeRegex.exec(content);
 
-    while (matches != null) {
-        var file = loaderUtils.urlToRequest(matches[1]);
-        var basename = path.basename(file);
-        var dirname = path.join(root, path.dirname(file));
-        var rawContent = readFile(basename, dirname);
-        content = content.replace(matches[0], rawContent);
-        matches = includeRegex.exec(content);
-    }
+		while (matches != null) {
+			var file = loaderUtils.urlToRequest(matches[1]);
+			var rawContent = readFile(path.basename(file), path.join(root, path.dirname(file)));
+			content = content.replace(matches[0], rawContent);
+			matches = includeRegex.exec(content);
+		}
 
-    return content;
-}
-
-function readFile(filepath, root) {
-    var self = readFile;
-
-    if (typeof(self.buffer) == "undefined") {
-        self.buffer = {};
-    }
-
-    if (filepath in self.buffer) {
-        return self.buffer[filepath];
-    }
-    
-    var content = resolveContent(fs.readFileSync(path.join(root, filepath), 'utf8'), root);    
-    self.buffer[filepath] = content;
-    return self.buffer[filepath];
-}
-
-module.exports = function(content) {
-    this.cacheable && this.cacheable();
-    var callback = this.async();
-    content = resolveContent(content, this.context);
-    var fn = _.template(content);
-    callback(null, "module.exports = " + fn.source + ";");
-};
+		return content;
+	};
+	
+	return function(content) {
+		this.cacheable && this.cacheable();
+		var callback = this.async();
+		content = readContent(content, this.context);
+		var fn = _.template(content);
+		callback(null, "module.exports = " + fn.source + ";");
+	};
+}();
 
 module.exports._ = _;
