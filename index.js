578d7e7ca3f4085a3174a31d34a92c9239c5d0de
emaphp <emmanuel.antico@gmail.com>
2015-08-19 12:39:50 -0300

    added: macro support, deprecated: include tag

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-var path = require('path');
-var fs = require('fs');
-var url = require("url");
-var loaderUtils = require('loader-utils');
-var attributeParser = require('./parser');
-
 try {
     var _ = require('underscore');
 } catch (e) {
@@ -11,44 +5,33 @@ try {
 }
 
 module.exports = function() {
-    var includeRegex = /<@include\s+([\/\w\.]*?[\w]+\.[\w]+)>/g;
+    var path = require('path');
+    var loaderUtils = require('loader-utils');
 
-    // Returns a template file content
-    var readFile = function(filepath, root) {
-        var self = readFile;
-        self.buffer = self.buffer || {};
+    // Parsers
+    var attributeParser = require('./lib/attributeParser');
+    var macroParser = require('./lib/macroParser');
 
-        if (filepath in self.buffer) {
-            return self.buffer[filepath];
-        }
-
-        var content = readContent(fs.readFileSync(path.join(root, filepath), 'utf8'), root);
-        self.buffer[filepath] = content;
-        return self.buffer[filepath];
-    };
-
-    // Parses an external file content
-    var readContent = function(content, root) {
-        var matches = includeRegex.exec(content);
-
-        while (matches != null) {
-            var file = loaderUtils.urlToRequest(matches[1]);
-            var rawContent = readFile(path.basename(file), path.join(root, path.dirname(file)));
-            content = content.replace(matches[0], rawContent);
-            matches = includeRegex.exec(content);
-        }
-
-        return content;
-    };
+    // Extendable arguments
+    var macros = _.extend({}, require('./lib/macros'));
 
     return function(content) {
+        this.cacheable && this.cacheable();
+        var callback = this.async();
+
+        // Default arguments
+        var root,
+            parseMacros = true,
+            attributes = ['img:src'];
+
+        // Parse arguments
         var query = loaderUtils.parseQuery(this.query);
-        var root = query.root;
-        var attributes = ['img:src'];
 
         if (_.isObject(query)) {
+            root = query.root;
+
             // Apply template settings
-            _.each(_.pick(query, 'interpolate', 'escape', 'evaluate', 'attributes', 'prependFilenameComment'), function(value, key) {
+            _.each(_.pick(query, 'interpolate', 'escape', 'evaluate'), function(value, key) {
                 _.templateSettings[key] = new RegExp(value, 'g');
             });
 
@@ -57,79 +40,48 @@ module.exports = function() {
                 attributes = _.isArray(query.attributes) ? query.attributes : [];
             }
 
-            // Set include regex
-            if (query.includeRegex !== undefined) {
-                includeRegex = new RegExp(query.includeRegex, 'g');
+            // Parse / ignore macros
+            if (query.parseMacros !== undefined) {
+                parseMacros = !!query.parseMacros;
+            }
+
+            // Prepend a html comment with the filename in it
+            if (query.prependFilenameComment) {
+                var filename = loaderUtils.getRemainingRequest(this);
+                var filenameRelative = path.relative(query.prependFilenameComment, filename);
+                content = "\n<!-- " + filenameRelative + "  -->\n" + content;
             }
         }
 
-        // Generates a random string for further proccessing
-        var randomIdent = function() {
-            return "@@@URL" + Math.random() + "@@@";
-        };
-
-        // Obtain external resource links
-        var links = attributeParser(content, function(tag, attr) {
-            return attributes.indexOf(tag + ':' + attr) >= 0;
-        });
-        links.reverse();
-
-        // Parse external resources
-        var data = {};
-        content = [content];
-        links.forEach(function(link) {
-            // Ignore absolute paths
-            if (/^\//.exec(link.value) && root == false) {
-                return;
-            }
-
-            if (!loaderUtils.isUrlRequest(link.value, root)) {
-                return;
-            }
-
-            var uri = url.parse(link.value);
-            if (uri.hash !== null && uri.hash !== undefined) {
-                uri.hash = null;
-                link.value = uri.format();
-                link.length = link.value.length;
-            }
-
-            do {
-                var ident = randomIdent();
-            } while (data[ident]);
-
-            data[ident] = link.value;
-            var x = content.pop();
-            content.push(x.substr(link.start + link.length));
-            content.push(ident);
-            content.push(x.substr(0, link.start));
-        });
-        content.reverse();
-        content = content.join('');
-
-        this.cacheable && this.cacheable();
-        var callback = this.async();
-
-        // Read file content
-        content = readContent(content, this.context);
-
-        // Prepend a html comment with the filename in it
-        if (query.prependFilenameComment) {
-            var filename = loaderUtils.getRemainingRequest(this);
-            var filenameRelative = path.relative(query.prependFilenameComment, filename);
-
-            content = "\n<!-- " + filenameRelative + "  -->\n" + content;
+        // Include additional macros
+        if (_.isObject(this.options.macros)) {
+            _.extend(macros, this.options.macros);
         }
 
-        // Replace random generated strings with require
+        // Parse macros
+        if (parseMacros) {
+            var macrosContext = macroParser(content, function (macro) {
+                return _.isFunction(macros[macro]);
+            }, 'MACRO');
+            content = macrosContext.replaceMatches(content);
+        }
+
+        // Parse attributes
+        var attributesContext = attributeParser(content, function (tag, attr) {
+            return attributes.indexOf(tag + ':' + attr) != -1;
+        }, 'ATTRIBUTE', root);
+        content = attributesContext.replaceMatches(content);
+
+        // Compile template
         var source = _.template(content).source;
-        content = source.replace(/@@@URL[0-9\.]+@@@/g, function(match) {
-            if (!data[match]) {
-                return match;
-            }
 
-            return "' + require(" + JSON.stringify(loaderUtils.urlToRequest(data[match], root)) + ") + '";
-        });
+        // Resolve macros
+        if (parseMacros) {
+            source = macrosContext.resolveMacros(source, macros);
+        }
+
+        // Resolve attributes
+        source = attributesContext.resolveAttributes(source);
 
         callback(null, "module.exports = " + content + ";");
     };
